78ae93b37d202c02d1f65edc6ae98a630501fd32
vvthai10 <vuvanthai1410@gmail.com>
2023-11-09 14:56:19 +0700

    Update UI Game

--- Assets/Scripts/Main Character/MainCharacter.cs
+++ Assets/Scripts/Main Character/MainCharacter.cs
@@ -145,6 +145,9 @@ public class main_character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(ControlOptions.Instance.CheckOpen()){
+            return;
+        }
         Debug.Log("Fps: " + 1.0f / Time.deltaTime);
 
         int meetEdge = meetEdgeAndFall();
