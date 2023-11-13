f60fdf713914899f05c2a9d406ebb939da608258
tuong2002 <tuong7b000@gmail.com>
2023-11-13 14:53:39 +0700

    fix_bug_enemies

--- Assets/Scripts/Enemies/enimies/enemy_damage.cs
+++ Assets/Scripts/Enemies/enimies/enemy_damage.cs
@@ -22,7 +22,7 @@ public class enemy_damage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.P))
         {
             Debug.Log("key click");
             TakeDamage(25);
